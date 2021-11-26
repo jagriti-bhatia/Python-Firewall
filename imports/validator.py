adb0bb9af63186ade44a01e5e82f8b44664a9e7e
apoorva-ak <93595495+apoorva-ak@users.noreply.github.com>
2021-11-26 21:28:35 +0530

    Update validator.py

--- imports/validator.py
+++ imports/validator.py
@@ -1,4 +1,3 @@
-#import os
 import csv
 
 def compare_rules(primary_rule: str, secondary_rules: list):
@@ -8,26 +7,20 @@ def compare_rules(primary_rule: str, secondary_rules: list):
             result.append(True)
         else:
             result.append(False)
-        
     return any(result)
 
 def validate_with_route_table(src_addr, dst_addr, src_port, dst_port):
-    try:
-        rules_stream = open("./imports/Rules.csv", "r")
-        rules = csv.reader(rules_stream)
-
-        for rule in rules:
-            # <SRC_IP> <SRC_PORT> <DST_IP> <DST_PORT>
-            # check for IP
-            if compare_rules(rule[1], [src_addr, dst_addr, "any"]) and compare_rules(rule[3], [dst_addr,src_addr, "any"]):
-                # check for port
-                if compare_rules(rule[2], [src_port, "any", 0]) and compare_rules(rule[4], [dst_port, "any", 0]):
-                    if str(rule[0]).lower() == "allow":
-                        return True
-                    elif str(rule[0]).lower() == "deny" or str(rule[0]).lower() == "disable":
-                        continue
-        return False
-        # 😂🍆
-    except Exception as e:
-        print(f"[ERR] Error reading rules: {e}")
-        return False
+    rules_stream = open("./imports/Rules.csv", "r")
+    rules = csv.reader(rules_stream)
+    for rule in rules:
+        # <SRC_IP> <SRC_PORT> <DST_IP> <DST_PORT>
+        # check for IP
+        if compare_rules(rule[1], [src_addr, "any"]) and compare_rules(rule[3], [dst_addr, "any"]):
+            # check for port
+            if compare_rules(rule[2], [src_port, "any"]) and compare_rules(rule[4], [dst_port, "any"]):
+                if str(rule[0]).lower() == "allow":
+                    return True
+                elif str(rule[0]).lower() == "deny":
+                    continue
+    return False
+    
